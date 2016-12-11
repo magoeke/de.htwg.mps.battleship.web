85a1b0fbb4075fe5a8f54bbac1a02c4943370356
magoeke <magoeke@htwg-konstanz.de>
2016-12-11 16:43:37 +0100

    Lobby without communication to server is working now.

--- de-htwg-mps-battleship/app/controllers/PolymerElementsHandler.scala
+++ de-htwg-mps-battleship/app/controllers/PolymerElementsHandler.scala
@@ -5,6 +5,7 @@ import play.api.mvc.{Action, Controller}
 
 class PolymerElementHandler extends Controller {
     val basePath = System.getenv("PWD") + "/target/web/public/main/lib/github-com-PolymerElements-"
+    val secondPath = System.getenv("PWD") + "/target/web/public/main/lib/github-com-polymerelements-"
     
     def redirectToElement(polymerPath: String) = Action {
 	//println(System.getenv("PWD"))
@@ -14,7 +15,13 @@ class PolymerElementHandler extends Controller {
               val fileString = scala.io.Source.fromInputStream(fileStream).mkString("")
               Ok(fileString) as HTML
          } catch {
-              case _ : Throwable => NotFound(polymerPath)
+             case _ : Throwable => {try {
+                 val stream: java.io.InputStream = new java.io.FileInputStream(secondPath + polymerPath)
+                 val fileString = scala.io.Source.fromInputStream(stream).mkString("")
+                 Ok(fileString) as HTML
+             } catch {
+                 case _ : Throwable => NotFound(polymerPath)
+             }}
          }
          //println(polymerPath)
          //Ok(polymerPath)
