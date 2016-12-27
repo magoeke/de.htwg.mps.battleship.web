3a78a70f0a9e8d603819869bd59cab515e0bc148
magoeke <magoeke@htwg-konstanz.de>
2016-12-27 12:55:23 +0100

    Removed outcommented code and println.

--- de-htwg-mps-battleship/app/controllers/PolymerElementsHandler.scala
+++ de-htwg-mps-battleship/app/controllers/PolymerElementsHandler.scala
@@ -8,8 +8,6 @@ class PolymerElementHandler extends Controller {
     val secondPath = System.getenv("PWD") + "/target/web/public/main/lib/github-com-polymerelements-"
     
     def redirectToElement(polymerPath: String) = Action {
-	//println(System.getenv("PWD"))
-	//println(polymerPath)
          try {
               val fileStream: java.io.InputStream = new java.io.FileInputStream(basePath + polymerPath)
               val fileString = scala.io.Source.fromInputStream(fileStream).mkString("")
@@ -23,14 +21,9 @@ class PolymerElementHandler extends Controller {
                  case _ : Throwable => NotFound(polymerPath)
              }}
          }
-         //println(polymerPath)
-         //Ok(polymerPath)
-         //Ok(routes.Assets.at("lib/github-com-PolymerElements-"+polymerPath))
-         //Redirect(routes.Assets.at("lib/github-com-PolymerElements-"+polymerPath))
     }
 
     def getPolymer(path: String) = Action {
-	println(path)
         Redirect(routes.Assets.at("lib/polymer/"+path))
     }
 }
