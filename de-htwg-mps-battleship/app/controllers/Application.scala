d3bd2bbcae5db4523433215887e5b27d33c2cc50
magoeke <magoeke@htwg-konstanz.de>
2016-12-27 13:01:45 +0100

    Outsourced websocket in own controller.

--- de-htwg-mps-battleship/app/controllers/Application.scala
+++ de-htwg-mps-battleship/app/controllers/Application.scala
@@ -4,30 +4,11 @@ import javax.inject.{Inject}
 
 import play.api._
 import play.api.mvc._
-import play.api.libs.streams._
 
-import akka.actor._
-import akka.stream.Materializer
-
-class Application @Inject() (implicit system: ActorSystem,
-                             materializer: Materializer,
-                             webJarAssets: WebJarAssets) extends Controller {
+class Application @Inject() (webJarAssets: WebJarAssets) extends Controller {
 
   def index = Action {
     Ok(views.html.Application.index(webJarAssets))
   }
 
-  def socket = WebSocket.accept[String, String] { request =>
-    ActorFlow.actorRef(out => MyWebSocketActor.props(out))
-  }
-}
-
-object MyWebSocketActor {
-  def props(out: ActorRef) = Props(new MyWebSocketActor(out))
-}
-
-class MyWebSocketActor(out: ActorRef) extends Actor {
-  override def receive = {
-    case msg: String => out ! ("I reeived yout message: " + msg)
-  }
 }
