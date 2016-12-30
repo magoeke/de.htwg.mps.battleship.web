0f2c69da9c84313190f17228d7d7b126dd3ce277
magoeke <magoeke@htwg-konstanz.de>
2016-12-30 15:03:34 +0100

    Draws board based on 2d vector

--- de-htwg-mps-battleship/app/assets/clojurescript/src/cljs/modern_cljs/core.cljs
+++ de-htwg-mps-battleship/app/assets/clojurescript/src/cljs/modern_cljs/core.cljs
@@ -10,18 +10,32 @@
 
 (def number-of-players 2)
 (def board-size 10)
+(def empty-board
+  (vec (repeat board-size
+    (vec (repeat board-size :empty)))))
+
+(defn game-cell
+  []
+  [:div {:class "game-cell"
+         :on-click #(println "clicked")}])
 
 (defn board
-  []
-  [:div "Gameboard"])
+  [state]
+  (println state)
+  [:div
+    (for [row state]
+      [:div {:class "game-row"}
+        (for [cell row] (game-cell))])])
+
+; ^{:key cell} (game-cell)
 
 (defn split-screen
   []
   (let [rows (.ceil js/Math (/ number-of-players 2))]
   [:div {:style {:height "100%"}}
     (repeat rows [:div {:style {:height (str (/ 100 rows) "%")}}
-      [:div {:class "left-screen"} (board)]
-      [:div {:class "right-screen"} (board)]])]))
+      [:div {:class "left-screen"} (board empty-board)]
+      [:div {:class "right-screen"} (board empty-board)]])]))
 
 (defn start []
   (r/render-component
