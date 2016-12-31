bd7d1b5afbf8c2d8f421680e85016595bfce2405
magoeke <magoeke@htwg-konstanz.de>
2016-12-31 14:37:43 +0100

    Generated atom that holds game-state.

--- de-htwg-mps-battleship/app/assets/clojurescript/src/cljs/modern_cljs/core.cljs
+++ de-htwg-mps-battleship/app/assets/clojurescript/src/cljs/modern_cljs/core.cljs
@@ -8,31 +8,41 @@
 (def number-of-players 2)
 (def board-size 10)
 (def empty-board (repeat (* board-size board-size) :empty))
+(defn initial-game [] (map (fn [] empty-board) (range number-of-players)))
 
-(defn board-with-index [board] (map (fn [a b] (vector a b)) board (range (count board))))
+(def game-state (r/atom (initial-game)))
+
+(defn list-with-index [board] (map (fn [a b] (vector a b)) board (range (count board))))
 (defn number-of-rows [] (.ceil js/Math (/ number-of-players 2)))
-(defn game-cell [] [:div {:class "game-cell" :on-click #(println "clicked")}])
 
-(defn output-game []
-  (let [cells (board-with-index empty-board)]
+(defn mousemove [evt]
+    (println (.-target evt)))
+
+(defn cell-class [state]
+  (cond
+    (= state :empty) "empty"
+    (= state :set) "set"
+    (= state :ht) "hit"))
+
+(defn game-cell [state] [:div {:class (str "game-cell" " " (cell-class state))
+                              :on-mouseMove mousemove}])
+
+(defn output-game [board]
+  (let [cells (list-with-index board)]
     [:div {:class "board"}
-      (doall (map (fn [cell] ^{:key (str "cell" (get cell 1))}[game-cell]) cells))]))
+      (doall (map (fn [cell] ^{:key (str "cell" (get cell 1))} [game-cell (get cell 0)]) cells))]))
 
 (defn split-screen
   []
   (let [rows (number-of-rows)]
     [:div {:style {:height "100vh"}}
-      (for [index (range number-of-players)]
-        [:div {:key (str "screen" index)
+      (for [board (list-with-index @game-state)]
+        [:div {:key (str "screen" (get board 1))
                :style {:height (str (/ 100 rows) "vh")}
-               :class (if (= 0 (mod index 2)) "left-screen" "right-screen")}
-          [output-game]
-          ])]))
+               :class (if (= 0 (mod (get board 1) 2)) "left-screen" "right-screen")}
+          [output-game (get board 0)]])]))
 
-(defn start []
-  (r/render-component
-   [split-screen]
-   (.getElementById js/document "content")))
+(defn start [] (r/render-component [split-screen] (.getElementById js/document "content")))
 
 ;; shows
 (start)
